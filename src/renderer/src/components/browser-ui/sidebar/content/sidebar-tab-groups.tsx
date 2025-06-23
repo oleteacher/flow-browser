d7fd8daf0b1f882dbd08a78698c981b6f211c69a
Evan <47493765+iamEvanYT@users.noreply.github.com>
2025-06-23 02:10:11 +0100

    Revert "Better Asleep Tabs (#128)" (#135)
    This reverts commit 4840cea9e119c715cc4dfbf5449a788455066a13.

--- src/renderer/src/components/browser-ui/sidebar/content/sidebar-tab-groups.tsx
+++ src/renderer/src/components/browser-ui/sidebar/content/sidebar-tab-groups.tsx
@@ -99,12 +99,11 @@ export function SidebarTab({ tab, isFocused }: { tab: TabData; isFocused: boolea
         isFocused && "bg-white dark:bg-white/25",
         isFocused && "active:bg-white active:dark:bg-white/25",
         "text-gray-900 dark:text-gray-200",
-        "transition-colors",
-        tab.asleep && "grayscale"
+        "transition-colors"
       )}
       initial={{ opacity: 0, x: -10 }}
       animate={{
-        opacity: tab.asleep ? 0.5 : 1,
+        opacity: 1,
         x: 0,
         scale: isPressed ? 0.985 : 1
       }}
