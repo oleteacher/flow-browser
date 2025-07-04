ddef06aa2cc8af6e654e538069b0d46fb95f7bb6
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-07-05 00:06:29 +0100

    fix: popup windows ui not working

--- src/renderer/src/routes/popup-ui/config.tsx
+++ src/renderer/src/routes/popup-ui/config.tsx
@@ -1,9 +1,10 @@
+import { PortalsProvider } from "@/components/portal/provider";
 import { RouteConfigType } from "@/types/routes";
 import { ReactNode } from "react";
 
 export const RouteConfig: RouteConfigType = {
   Providers: ({ children }: { children: ReactNode }) => {
-    return children;
+    return <PortalsProvider>{children}</PortalsProvider>;
   },
   Fallback: null
 };
